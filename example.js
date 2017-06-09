29b810b26fd34a479e39d66f2f5595388310119f
Adrian Schaedle <adrian@schaedle.me>
2017-06-09 13:20:19 -0400

    Separate example from library code
    This change separates index.js into two files: one that has just the
    drawing functions and one that has the example drawing.
    
    It gets rid of the individual drawLine function in favor of one that
    draws an array of lines. The overhead of calling getImageData and
    putImageData between lines is just too high.
    
    Small change to the example as well, it now draws against the whole
    viewport.

---
+++ example.js
@@ -0,0 +1,80 @@
+var { drawLine, drawLines } = require(".");
+
+var SIDES = {
+  TOP: "TOP",
+  RIGHT: "RIGHT",
+  BOTTOM: "BOTTOM",
+  LEFT: "LEFT",
+};
+
+var canvas = document.createElement("canvas");
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+document.body.style.margin = 0;
+document.body.style.overflow = "hidden";
+document.body.append(canvas);
+
+var ctx = canvas.getContext("2d");
+
+requestAnimationFrame(draw);
+
+function draw(timestamp) {
+  benchmark("batched time", function () {
+    var lines = [];
+    for (var i = 0; i < 500; i++) {
+      lines.push(randomLine(ctx.canvas));
+    }
+    drawLines(ctx, lines);
+  });
+
+  // requestAnimationFrame(main);
+}
+
+var blue = [100, 100, 100, 255];
+
+function randomLine(dimensions, length) {
+  return {
+    start: randomPointAlongEdge(dimensions),
+    end: randomPointAlongEdge(dimensions),
+    color: blue
+  };
+}
+
+function randomPointAlongEdge(dimensions) {
+  switch (randomSide()) {
+    case SIDES.TOP:
+      return { x: randomInteger(dimensions.width), y: 0 };
+    case SIDES.RIGHT:
+      return { x: dimensions.width, y: randomInteger(dimensions.height) };
+    case SIDES.BOTTOM:
+      return { x: randomInteger(dimensions.width), y: dimensions.height };
+    case SIDES.LEFT:
+      return { x: 0, y: randomInteger(dimensions.height) };
+  }
+}
+
+function randomSide() {
+  return Object.keys(SIDES)[Math.floor(Math.random() * 4)];
+}
+
+function randomPoint(dimensions) {
+  return {
+    x: randomInteger(dimensions.width),
+    y: randomInteger(dimensions.height)
+  };
+}
+
+function randomInteger(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function clear(context) {
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+}
+
+function benchmark(message, fn) {
+  var start = performance.now();
+  fn();
+  var end = performance.now();
+  console.log(message, end - start, "(ms)");
+}
--- index.js
+++ index.js
@@ -1,65 +1,27 @@
-var SIDES = {
-  TOP: "TOP",
-  RIGHT: "RIGHT",
-  BOTTOM: "BOTTOM",
-  LEFT: "LEFT",
+module.exports = {
+  drawLines,
+  drawLine,
+  setPixel
 };
-
-main(document.body);
-
-function main(parent) {
-  var canvas = document.createElement("canvas");
-
-  canvas.width = 1000;
-  canvas.height = 1000;
-
-  parent.append(canvas);
-
-  var ctx = canvas.getContext("2d");
-
-  requestAnimationFrame(draw);
-
-  function draw(timestamp) {
-    var start = performance.now();
-    for (var i = 0; i < 500; i++) {
-      drawLine(ctx, randomLine(ctx.canvas));
-    }
-    var end = performance.now();
-    console.log("individual image data time (ms)", end - start);
-
-
-    var lines = []
-    for (var i = 0; i < 500; i++) {
-      lines.push(randomLine(ctx.canvas));
-    }
-    start = performance.now();
-    drawLines(ctx, lines);
-    end = performance.now();
-    console.log("batched time (ms)", end - start);
-
-    // requestAnimationFrame(main);
-  }
-}
-
-var blue = [100, 100, 100, 255];
 
 function drawLines(context, lines) {
   var imageData = getImageData(context);
-  var drawPixel = setPixel.bind(null, imageData, blue);
 
-  lines.forEach(function ({ start, end }) {
-    bresenhamPoints(start.x, start.y, end.x, end.y).forEach(drawPixel);
+  lines.forEach(function (line) {
+    drawLine(imageData, line);
   });
 
   putImageData(context, imageData);
 }
 
-function drawLine(context, line) {
-  var imageData = getImageData(context);
-  var drawPixel = setPixel.bind(null, imageData, blue);
-  var { start, end } = line
-  bresenhamPoints(start.x, start.y, end.x, end.y).forEach(drawPixel);
-  putImageData(context, imageData);
+function drawLine(imageData, line) {
+  const { start, end, color } = line;
+
+  bresenhamPoints(start.x, start.y, end.x, end.y).forEach(function (point) {
+    setPixel(imageData, color, point);
+  });
+
+  return imageData;
 }
 
 function setPixel(imageData, color, point) {
@@ -72,6 +34,7 @@ function setPixel(imageData, color, point) {
   data[offset + 1] = g;
   data[offset + 2] = b;
   data[offset + 3] = a;
+
   return imageData;
 }
 
@@ -83,41 +46,6 @@ function getImageData(context) {
 function putImageData(context, imageData) {
   context.putImageData(imageData, 0, 0);
   return context;
-}
-
-function randomLine(dimensions, length) {
-  return {
-    start: randomPointAlongEdge(dimensions),
-    end: randomPointAlongEdge(dimensions)
-  };
-}
-
-function randomPointAlongEdge(dimensions) {
-  switch (randomSide()) {
-    case SIDES.TOP:
-      return { x: randomInteger(dimensions.width), y: 0 };
-    case SIDES.RIGHT:
-      return { x: dimensions.width, y: randomInteger(dimensions.height) };
-    case SIDES.BOTTOM:
-      return { x: randomInteger(dimensions.width), y: dimensions.height };
-    case SIDES.LEFT:
-      return { x: 0, y: randomInteger(dimensions.height) };
-  }
-}
-
-function randomSide() {
-  return Object.keys(SIDES)[Math.floor(Math.random() * 4)];
-}
-
-function randomPoint(dimensions) {
-  return {
-    x: randomInteger(dimensions.width),
-    y: randomInteger(dimensions.height)
-  };
-}
-
-function randomInteger(max) {
-  return Math.floor(Math.random() * max);
 }
 
 function bresenhamPoints(x0, y0, x1, y1) {
